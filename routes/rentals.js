74267d4b2825e14643f1bc472e20a24d8235e810
herrifransisca <herrifransisca@gmail.com>
2018-11-28 17:47:05 +0700

    09-08- Transactions

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,12 @@
 const { Movie } = require('../models/movie');
 const { Customer } = require('../models/customer');
 const { Rental, validate } = require('../models/rental');
+const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
@@ -34,12 +38,23 @@ router.post('/', async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate
     }
   });
-  rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
+  try {
+    new Fawn.Task()
+      .save('rentals', rental)
+      .update(
+        'movies',
+        { _id: movie._id },
+        {
+          $inc: { numberInStock: -1 }
+        }
+      )
+      .run();
 
-  res.send(rental);
+    res.send(rental);
+  } catch (ex) {
+    return res.status(500).send('Something failed.');
+  }
 });
 
 router.get('/:id', async (req, res) => {
