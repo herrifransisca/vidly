225517ea142e47b8184af705aba5c953c85b4df9
herrifransisca <herrifransisca@gmail.com>
2018-11-29 12:55:08 +0700

    10-02- Creating the User Model (mosh's code)

--- models/user.js
+++ models/user.js
@@ -7,8 +7,8 @@ const User = mongoose.model(
     name: {
       type: String,
       required: true,
-      minlength: 5,
-      maxlength: 50
+      min: 5,
+      max: 50
     },
     email: {
       type: String,
@@ -28,9 +28,19 @@ const User = mongoose.model(
 
 function validateUser(user) {
   const schema = {
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required()
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
   };
 
   return Joi.validate(user, schema);
