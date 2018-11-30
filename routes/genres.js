4a1a80c83cf6380857464317d5cbc11a3035d733
herrifransisca <herrifransisca@gmail.com>
2018-11-30 18:45:38 +0700

    11-04- Removing Try Catch Blocks

--- routes/genres.js
+++ routes/genres.js
@@ -1,27 +1,31 @@
+const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  try {
+router.get(
+  '/',
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort('name');
     res.send(genres);
-  } catch (ex) {
-    next(ex);
-  }
-});
+  })
+);
 
-router.post('/', auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) res.status(400).send(error.details[0].message);
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) res.status(400).send(error.details[0].message);
 
-  const genre = new Genre({ name: req.body.name });
-  await genre.save();
+    const genre = new Genre({ name: req.body.name });
+    await genre.save();
 
-  res.send(genre);
-});
+    res.send(genre);
+  })
+);
 
 router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body);
