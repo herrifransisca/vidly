379755b9b76b7c29a887cfebf5f577660157a1df
herrifransisca <herrifransisca@gmail.com>
2018-11-30 19:21:23 +0700

    11-05- Express Async Errors

--- routes/genres.js
+++ routes/genres.js
@@ -1,31 +1,23 @@
-const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
 const express = require('express');
 const router = express.Router();
 
-router.get(
-  '/',
-  asyncMiddleware(async (req, res) => {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
-  })
-);
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
+});
 
-router.post(
-  '/',
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) res.status(400).send(error.details[0].message);
+router.post('/', auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) res.status(400).send(error.details[0].message);
 
-    const genre = new Genre({ name: req.body.name });
-    await genre.save();
+  const genre = new Genre({ name: req.body.name });
+  await genre.save();
 
-    res.send(genre);
-  })
-);
+  res.send(genre);
+});
 
 router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body);
