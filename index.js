d88c9880d8124ae2ab7a42fc414922c30b6d942f
herrifransisca <herrifransisca@gmail.com>
2018-12-01 22:43:48 +0700

    11-09- Unhandled Promise Rejections- bestPractice, terminate node proces

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ const app = express();
 process.on('uncaughtException', ex => {
   console.log('WE GOT AN UNCAUGHT EXCEPTION');
   winston.error(ex.message, ex);
+  process.exit(1);
 });
 
 process.on('unhandledRejection', ex => {
   console.log('WE GOT AN UNHANDLED REJECTION');
   winston.error(ex.message, ex);
+  process.exit(1);
 });
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
