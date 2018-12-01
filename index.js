a11137ee29f73229498e639783445065c054b421
herrifransisca <herrifransisca@gmail.com>
2018-12-01 22:54:16 +0700

    11-09- Unhandled Promise Rejections (diff way unhandledPromiseRejection)

--- index.js
+++ index.js
@@ -28,20 +28,24 @@ winston.handleExceptions(
   new winston.transports.File({ filename: 'uncaughtExceptions.log' })
 );
 
+// way #1 for "unhandled promise rejection"
+// process.on('unhandledRejection', ex => {
+//   winston.error(ex.message, ex);
+//   process.exit(1);
+// });
+
+// way #2 for "unhandled promise rejection"
 process.on('unhandledRejection', ex => {
-  console.log('WE GOT AN UNHANDLED REJECTION');
-  winston.error(ex.message, ex);
-  process.exit(1);
+  throw ex;
 });
 
 winston.add(winston.transports.File, { filename: 'logfile.log' });
 winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
 
-throw new Error('Something failed during startup.'); // for uncaught exception
+// throw new Error('Something failed during startup.'); // for uncaught exception
 
-// const p = Promise.reject(new Error('Something failed miserably!'));
-// // p.then().catch()
-// p.then(() => console.log('Done')); // -> will have unhandled promise rejection
+const p = Promise.reject(new Error('Something failed miserably!'));
+p.then(() => console.log('Done')); // -> will have unhandled promise rejection
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined.');
