4b2be4a2535f66de465a087fd6bdb7123c27683c
herrifransisca <herrifransisca@gmail.com>
2018-12-04 22:27:27 +0700

    13-07- Validating ObjectIDs

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,7 @@
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -45,6 +46,9 @@ router.delete('/:id', [auth, admin], auth, async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(404).send('Invalid ID.');
+
   const genre = await Genre.findById(req.params.id);
 
   if (!genre)
