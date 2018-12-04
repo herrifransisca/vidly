c3512479b9086b1b50bc120b81581ae534877024
herrifransisca <herrifransisca@gmail.com>
2018-12-04 15:55:12 +0700

    13-02- Preparing the App

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-require('winston-mongodb');
+// require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
@@ -8,10 +8,10 @@ module.exports = function() {
     new winston.transports.File({ filename: 'uncaughtExceptions.log' })
   );
 
-  process.on('unhandledRejection', ex => {
+  process.on('unhandledRejection', (ex) => {
     throw ex;
   });
 
   winston.add(winston.transports.File, { filename: 'logfile.log' });
-  winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
+  // winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
 };
