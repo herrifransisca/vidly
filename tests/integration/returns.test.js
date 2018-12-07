76d9ae10767d59c262c336a7d4c16b3596400aab
herrifransisca <herrifransisca@gmail.com>
2018-12-07 09:54:05 +0700

    14-03- Test Cases

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,3 +1,16 @@
+// POST /api/returns  { customerId, movieId }
+
+// Returns 401 if client is not logged in
+// Returns 400 if customerId is not provided
+// Returns 400 if movieId is not provided
+// Returns 404 if no rental found for this customer/movie
+// Returns 400 if rental already processed
+// Returns 200 if valid request
+// Set the return date
+// Calculate the rental fee
+// Increase the stock
+// Return the rental
+
 const { Rental } = require('../../models/rental');
 const mongoose = require('mongoose');
 
